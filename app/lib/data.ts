927af9d73643838c5db278793a07bbf7e63072f2
Cy Caruso <ccaruso@idealss.com>
2025-09-17 11:28:35 -0700

    customers data

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -28,6 +28,24 @@ export async function fetchRevenue() {
   }
 }
 
+export async function altFetchRevenue() {
+  try {
+    // Artificially delay a response for demo purposes.
+    // Don't do this in production :)
+
+    console.log('Fetching revenue data...');
+    await new Promise((resolve) => setTimeout(resolve, 7000));
+
+    const data = await sql<Revenue>`SELECT * FROM revenue`;
+
+    console.log('Data fetch completed after 7 seconds.');
+    return data.rows;
+  } catch (error) {
+    console.error('Database Error:', error);
+    throw new Error('Failed to fetch revenue data.');
+  }
+}
+
 export async function fetchLatestInvoices() {
   try {
     //await new Promise((resolve) => setTimeout(resolve, 2000));
